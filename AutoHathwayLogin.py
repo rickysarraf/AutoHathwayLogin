da95c3343d8b0554821c6c8756c9b29d155839e0
Ritesh Raj Sarraf <rrs@researchut.com>
2014-10-07 16:35:33 +0530

    Update to what I have been using

--- AutoHathwayLogin.py
+++ AutoHathwayLogin.py
@@ -27,12 +27,29 @@ socket.setdefaulttimeout(sleepTime)
 hostname = ['login.hathway.com', '203.212.193.60', '203.212.193.61']
 url = "/bsp/login.do?action=doLoginSubmit&flowId=UserLogin&username=" + userName + "&password=" + password
 
+file=None
+if file is None:
+	#try:
+	fh = open("/tmp/hathway.log", 'w')
+	#except IOError:
+	#	print "Couldn't write to file /tmp/hathway.log";
+else:
+	try:
+		fh = open(file, 'w')
+	except IOError:
+		print "Couldn't write to file %s\n" % (file);
+
 def setup():
 	if len(sys.argv) > 1:
 		userName=sys.argv[1]
 		password=sys.argv[2]
 		sleepTime=sys.argv[3]
 	
+def local_write(str):
+	sys.stdout.write(str)
+	fh.write(str)
+	fh.flush()
+
 def checkInternetConnectivity():
 	x = 1
 	try:
@@ -50,20 +67,20 @@ def connectToHathway():
 		for host in hostname:
 			real_host = "http://" + host + url
 			try:
-				sys.stdout.write("Trying to authenticate to server %s\n" % (real_host) )
+				local_write("%s\tTrying to authenticate to server %s\n" % (time.ctime(), real_host) )
 				urllib2.urlopen(real_host)
 			except:
-				sys.stderr.write("Failed to connect to %s Retrying\n" % (real_host) )
+				local_write("%s\tFailed to connect to %s Retrying\n" % (time.ctime(), real_host) )
 				continue
 	
 setup()
 while 1==1:
 	if checkInternetConnectivity() == 0:
-		sys.stderr.write("Internet connectivity is dead. Trying to contact Hathway auth severs.\n")
+		local_write("%s\tInternet connectivity is dead. Trying to contact Hathway auth severs.\n" % (time.ctime() ) )
 		connectToHathway()
 		time.sleep(sleepTime)
 		continue
 	else:
-		sys.stdout.write("Internet connectivity alive. Sleeping for %s seconds.\n" % (sleepTime) )
+		local_write("%s\tInternet connectivity alive. Sleeping for %s seconds.\n" % (time.ctime(), sleepTime) )
 		time.sleep(sleepTime)
 		continue 
